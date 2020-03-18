82e2adc33cf19c48c624844e52e409f839b5bf24
Александр Яновер <a.yanover@tradesoft.ru>
2020-03-18 09:50:06 +0300

    добавление timestamp в качестве параметра у подключаемых файлов

--- config/web.php
+++ config/web.php
@@ -68,13 +68,22 @@ $config = [
                 'catalog/index' => 'catalog/index',
                 'catalog/generate-group-kdv' => 'catalog/generate-group-kdv',
                 'catalog/<categoryUrl:\S+>' => 'catalog/items'
-
-
             ],
         ],
         'assetManager' => [
             'linkAssets' => true,
+            'appendTimestamp' => true,
         ],
+    ],
+    'modules' => [
+        'gridview' =>  [
+            'class' => '\kartik\grid\Module',
+            // enter optional module parameters below - only if you need to
+            // use your own export download action or custom translation
+            // message source
+             'downloadAction' => 'gridview/export/download'
+            // 'i18n' => []
+        ]
     ],
     'params' => array_merge($params, $kdvParams),
 ];
